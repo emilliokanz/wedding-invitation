ca76368c6a51ed389bc7d209f7d90bd3c90ad886
emillio <emilliokanz@gmail.com>
2025-02-05 23:13:47 +0700

    a test update viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  server: {
+    host: true,
   },
 })
